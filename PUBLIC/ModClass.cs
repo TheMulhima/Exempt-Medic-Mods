ecd319f6477e7255c57be7042cca2b740c27516d
medicatedshawnie <60412657+medicatedshawnie@users.noreply.github.com>
2022-06-08 18:42:10 -0400

    Add files via upload

---
+++ PUBLIC/ModClass.cs
@@ -0,0 +1,39 @@
+﻿using Modding;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using HutongGames.PlayMaker;
+using HutongGames.PlayMaker.Actions;
+
+
+namespace Silent_Dreamshield
+{
+    class Silent_Dreamshield : Mod
+    {
+        public override string GetVersion() => "1.0";
+        public override void Initialize()
+        {
+            Log("Initializing");
+
+            On.PlayMakerFSM.OnEnable += OnFsmEnable;
+            
+            Log("Initialized");
+        }
+        private static void OnFsmEnable(On.PlayMakerFSM.orig_OnEnable orig, PlayMakerFSM self)
+        {
+            orig(self);
+
+            if (self.gameObject.name == "Shield" && self.FsmName == "Shield Hit")
+            {
+                ModifyFsm(self);
+            }
+        }
+        private static void ModifyFsm(PlayMakerFSM fsm)
+        {
+            FsmStateAction[] origActions = fsm.Fsm.GetState("Slash Anim").Actions;
+            FsmStateAction[] actions = new FsmStateAction[1];
+            actions[0] = origActions[0];
+            fsm.Fsm.GetState("Slash Anim").Actions = actions;
+        }
+    }
+}
